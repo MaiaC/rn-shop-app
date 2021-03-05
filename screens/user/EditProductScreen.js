7b13ebd80770b6e80d68a8b8b7dd0d3dd91f1415
Maia <maiaccog@gmail.com>
2021-03-06 09:54:58 +1000

    change line

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -5,7 +5,7 @@ const EditProductScreen = () => {
   return (
     <View>
       <Text>Edit Product Screen</Text>
-      <Text>Maia just added this line.</Text>
+      <Text>Maia just changed this line.</Text>
     </View>
   );
 };
